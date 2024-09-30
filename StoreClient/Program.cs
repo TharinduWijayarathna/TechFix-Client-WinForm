2527f59c6ac6313376ef3e3e4483ebefe4a9f3a8
TharinduWijayarathna <wikum.dev@gmail.com>
2024-10-01 02:59:29 +0530

    Login Form Added

--- StoreClient/Program.cs
+++ StoreClient/Program.cs
@@ -16,7 +16,7 @@ namespace StoreClient
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new HomeForm());
+            Application.Run(new LoginForm());
         }
     }
 }
