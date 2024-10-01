69a4715d10d1ef2d8664b7a6a486e0bd588ff32f
TharinduWijayarathna <wikum.dev@gmail.com>
2024-10-01 23:41:28 +0530

    Menu Form UI Changes

--- StoreClient/Form/HomeForm.Designer.cs
+++ StoreClient/Form/HomeForm.Designer.cs
@@ -33,13 +33,16 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(70, 92);
+            this.button1.Font = new System.Drawing.Font("Gabriola", 14.25F, System.Drawing.FontStyle.Bold);
+            this.button1.Location = new System.Drawing.Point(447, 136);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(232, 40);
+            this.button1.Size = new System.Drawing.Size(363, 40);
             this.button1.TabIndex = 0;
             this.button1.Text = "Manage Stock";
             this.button1.UseVisualStyleBackColor = true;
@@ -47,9 +50,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(70, 138);
+            this.button2.Font = new System.Drawing.Font("Gabriola", 14.25F, System.Drawing.FontStyle.Bold);
+            this.button2.Location = new System.Drawing.Point(447, 182);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(232, 40);
+            this.button2.Size = new System.Drawing.Size(363, 40);
             this.button2.TabIndex = 1;
             this.button2.Text = "Manage Quote Requests";
             this.button2.UseVisualStyleBackColor = true;
@@ -57,9 +61,10 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(70, 184);
+            this.button3.Font = new System.Drawing.Font("Gabriola", 14.25F, System.Drawing.FontStyle.Bold);
+            this.button3.Location = new System.Drawing.Point(447, 228);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(232, 40);
+            this.button3.Size = new System.Drawing.Size(363, 40);
             this.button3.TabIndex = 2;
             this.button3.Text = "Manage Orders";
             this.button3.UseVisualStyleBackColor = true;
@@ -67,9 +72,10 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(70, 276);
+            this.button4.Font = new System.Drawing.Font("Gabriola", 14.25F, System.Drawing.FontStyle.Bold);
+            this.button4.Location = new System.Drawing.Point(447, 360);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(232, 40);
+            this.button4.Size = new System.Drawing.Size(363, 52);
             this.button4.TabIndex = 3;
             this.button4.Text = "Exit";
             this.button4.UseVisualStyleBackColor = true;
@@ -77,19 +83,42 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(70, 230);
+            this.button5.Font = new System.Drawing.Font("Gabriola", 14.25F, System.Drawing.FontStyle.Bold);
+            this.button5.Location = new System.Drawing.Point(447, 274);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(232, 40);
+            this.button5.Size = new System.Drawing.Size(363, 40);
             this.button5.TabIndex = 4;
             this.button5.Text = "View Quotations";
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Gabriola", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(447, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(363, 62);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "TechFix Shop Menu";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Gabriola", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Image = global::StoreClient.Properties.Resources.menu;
+            this.label2.Location = new System.Drawing.Point(0, -2);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(435, 453);
+            this.label2.TabIndex = 7;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // HomeForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(379, 450);
+            this.ClientSize = new System.Drawing.Size(822, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
@@ -108,5 +137,7 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
